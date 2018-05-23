50d7714dbfb506333deae247fdbe220f1ceead82
Daniel Machin <jmdanny@gmail.com>
2018-05-23 10:11:56 -0400

    Searchby added
    Added ability to search by SerialNumber, DeviceID, or EASID

--- csvReport.py
+++ csvReport.py
@@ -10,15 +10,18 @@ parser = argparse.ArgumentParser(description='Cross reference csv file with AirW
 parser.add_argument("-csv", help='CSV file to import', type=str)
 parser.add_argument("-header", help='CSV column header', type=str)
 parser.add_argument("-output", "-out", help='Specify Output CSV file', type=str)
+parser.add_argument('-searchBy', help='Search by SerialNumber, DeviceID, or EASID', type=str)
 
 args = parser.parse_args()
 
 if args.csv:
-	api = airwatch()
-
 	if not args.header:
 		print('Please specify column header')
 		quit()
+	if not args.searchBy:
+		print('Please specify search type: SerialNumber, DeviceID, or EASID')
+		quit()
+	api = airwatch()
 	import csv
 	csvlist = []
 	outputlist = []
@@ -34,7 +37,14 @@ if args.csv:
 		progress += 1
 		sys.stdout.write('\rProcessing ' + str(progress) + ' of ' + str(listLength) + ' items.')
 		sys.stdout.flush()
-		search = api.getDeviceInformation(easID=item[args.header])
+		if args.searchBy.lower() == 'serialnumber' or args.searchBy.lower() == 'serial':
+			search = api.getDeviceInformation(serialNumber=item[args.header])
+		elif args.searchBy.lower() == 'deviceid':
+			search = api.getDeviceInformation(deviceID=item[args.header])
+		elif args.searchBy.lower() == 'easid':
+			search = api.getDeviceInformation(easID=item[args.header])
+		else:
+			print('Unknown search type: SerialNumber, DeviceID, or EASID')
 		newItem = {}
 		if not 'errorCode' in search.keys():
 			item['AirWatchDeviceID'] = str(search['Id']['Value'])
